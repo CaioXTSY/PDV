8fecd6ede565108058b85a587e3f62058c1184aa
CaioXTSY <caio.teixeira@arapiraca.ufal.br>
2023-07-18 23:17:20 -0300

    usable version
    - Json "Database"
    - Functional Interface
    - Working Funcions
    (no bug detected ;) )

--- pdv.py
+++ pdv.py
@@ -1,4 +1,5 @@
 import webview
+import json
 from threading import Thread
 
 class Produto:
@@ -7,21 +8,45 @@ class Produto:
         self.codigo = codigo
         self.preco = preco
 
+    def to_dict(self):
+        return {
+            "nome": self.nome,
+            "codigo": self.codigo,
+            "preco": self.preco
+        }
+
 class PDV:
     def __init__(self):
         self.produtos = {}
         self.venda_atual = []
+        self._load_from_file()
+
+    def _load_from_file(self):
+        try:
+            with open("produtos.json", "r") as file:
+                data = json.load(file)
+                for item in data:
+                    prod = Produto(item["nome"], item["codigo"], item["preco"])
+                    self.produtos[item["codigo"]] = prod
+        except FileNotFoundError:
+            pass
+
+    def _save_to_file(self):
+        with open("produtos.json", "w") as file:
+            json.dump([produto.to_dict() for produto in self.produtos.values()], file)
 
     def adicionar_produto(self, nome, codigo, preco):
         if float(preco) <= 0:
             return False
         else:
-            self.produtos[codigo] = Produto(nome, codigo, float(preco))
+            self.produtos[codigo] = Produto(nome, codigo, preco)
+            self._save_to_file()
             return True
 
     def remover_produto(self, codigo):
         if codigo in self.produtos:
             del self.produtos[codigo]
+            self._save_to_file()
             return True
         else:
             return False
@@ -30,11 +55,7 @@ class PDV:
         return [{"nome": produto.nome, "codigo": produto.codigo, "preco": produto.preco} for produto in self.produtos.values()]
 
     def iniciar_venda(self, codigo, quantidade):
-        try:
-            quantidade = int(quantidade)
-        except ValueError:
-            return False
-
+        quantidade = int(quantidade)
         if codigo in self.produtos and quantidade > 0:
             self.venda_atual.append((self.produtos[codigo], quantidade))
             return True
@@ -42,28 +63,36 @@ class PDV:
             return False
 
     def finalizar_venda(self):
-        total = sum(produto.preco * quantidade for produto, quantidade in self.venda_atual)
+        total = sum(float(produto.preco) * int(quantidade) for produto, quantidade in self.venda_atual)
         self.venda_atual = []
         return total
+    
+    def get_venda_atual(self):
+        return [{"nome": item[0].nome, "quantidade": item[1], "preco": item[0].preco} for item in self.venda_atual]
 
 pdv = PDV()
 
-def load_html(window):
+def load_html_with_assets(window):
     with open('index.html', 'r') as f:
-        html = f.read()
+        html_content = f.read()
 
     with open('styles.css', 'r') as f:
-        styles = f.read()
+        css_content = f.read()
 
-    with open('scripts.js', 'r') as f:
-        scripts = f.read()
-
-    full_html = html.replace('</head>', '<style>{}</style></head>'.format(styles))
-    full_html = full_html.replace('</body>', '<script>{}</script></body>'.format(scripts))
+    html_content = html_content.replace('<link rel="stylesheet" href="styles.css">', f'<style>{css_content}</style>')
     
-    window.load_html(full_html)
+    with open('scripts.js', 'r') as f:
+        js_content = f.read()
+        
+    html_content = html_content.replace('<script src="script.js"></script>', f'<script>{js_content}</script>')
+    
+    window.load_html(html_content)
+
+pdv = PDV()
+window = webview.create_window('PDV', js_api=pdv)
+thread = Thread(target=load_html_with_assets, args=(window,))
+thread.start()
+webview.start(debug=True)
 
 window = webview.create_window('PDV', js_api=pdv)
-thread = Thread(target=load_html, args=(window,))
-thread.start()
 webview.start(debug=True)
--- scripts.js
+++ scripts.js
@@ -1,89 +1,121 @@
 function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
+    var i, tabcontent, tablinks;
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-  }
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 
-  if (tabName == 'Listar') {
-      listarProdutos();
-  }
-  if (tabName == 'Vender') {
-      mostrarCarrinho();
-  }
+    if (tabName === "Listar") {
+        listarProdutos();
+    }
 }
 
-document.getElementById('Adicionar').style.display = 'block';
+function adicionarProduto() {
+    var nome = document.getElementById("nome").value;
+    var codigo = document.getElementById("codigo").value;
+    var preco = document.getElementById("preco").value;
 
-async function adicionarProduto() {
-  const nome = document.getElementById('nome').value;
-  const codigo = document.getElementById('codigo').value;
-  const preco = document.getElementById('preco').value;
+    if (nome && codigo && preco) {
+        var resultado = window.pywebview.api.adicionar_produto(nome, codigo, preco);
+        resultado.then(function (response) {
+            if (response === true) {
+                alert("Produto adicionado com sucesso!");
 
-  const resultado = await pywebview.api.adicionar_produto(nome, codigo, preco);
+                document.getElementById("nome").value = "";
+                document.getElementById("codigo").value = "";
+                document.getElementById("preco").value = "";
 
-  if (resultado) {
-      alert('Produto adicionado com sucesso!');
-      listarProdutos();
-  } else {
-      alert('Falha ao adicionar o produto.');
-  }
-
-  document.getElementById('nome').value = '';
-  document.getElementById('codigo').value = '';
-  document.getElementById('preco').value = '';
+            } else {
+                alert("Erro ao adicionar produto. Motivo: " + response);
+            }
+        });
+    } else {
+        alert("Preencha todos os campos!");
+    }
 }
 
-async function removerProduto() {
-  const codigo = document.getElementById('removerCodigo').value;
+function removerProduto() {
+    var codigo = document.getElementById("removerCodigo").value;
 
-  const resultado = await pywebview.api.remover_produto(codigo);
-
-  if (resultado) {
-      alert('Produto removido com sucesso!');
-      listarProdutos();
-  } else {
-      alert('Falha ao remover o produto.');
-  }
-
-  document.getElementById('removerCodigo').value = '';
+    if (codigo) {
+        var resultado = window.pywebview.api.remover_produto(codigo);
+        resultado.then(function (response) {
+            if (response) {
+                alert("Produto removido com sucesso!");
+            } else {
+                alert("Erro ao remover produto.");
+            }
+        });
+    } else {
+        alert("Informe o código do produto!");
+    }
 }
 
-async function listarProdutos() {
-  const produtos = await pywebview.api.listar_produtos();
+function iniciarVenda() {
+    var codigo = document.getElementById("vendaCodigo").value;
+    var quantidade = document.getElementById("quantidade").value;
 
-  let html = '';
-  for (let produto of produtos) {
-      html += `<p>${produto.nome} - ${produto.codigo} - ${produto.preco}</p>`;
-  }
-
-  document.getElementById('produtos').innerHTML = html;
+    if (codigo && quantidade) {
+        var resultado = window.pywebview.api.iniciar_venda(codigo, quantidade);
+        resultado.then(function (response) {
+            if (response) {
+                atualizarCarrinho();
+            } else {
+                alert("Erro ao adicionar ao carrinho.");
+            }
+        });
+    } else {
+        alert("Preencha todos os campos!");
+    }
 }
 
-async function iniciarVenda() {
-  const codigo = document.getElementById('vendaCodigo').value;
-  const quantidade = document.getElementById('quantidade').value;
+function atualizarCarrinho() {
+    var venda = window.pywebview.api.get_venda_atual();
+    venda.then(function (items) {
+        var carrinhoDiv = document.getElementById("carrinho");
+        var total = 0;
 
-  const resultado = await pywebview.api.iniciar_venda(codigo, quantidade);
+        carrinhoDiv.innerHTML = "";
+        items.forEach(item => {
+            var li = document.createElement("li");
+            li.textContent = item.nome + " (" + item.quantidade + ") - $" + item.preco;
+            carrinhoDiv.appendChild(li);
+            total += item.preco * item.quantidade;
+        });
 
-  if (resultado) {
-      mostrarCarrinho();
-  } else {
-      alert('Falha ao iniciar venda.');
-  }
-
-  document.getElementById('vendaCodigo').value = '';
-  document.getElementById('quantidade').value = '';
+        document.getElementById("totalVenda").textContent = "$" + total.toFixed(2);
+    });
 }
 
-function mostrarCarrinho() {
+function finalizarVenda() {
+    var resultado = window.pywebview.api.finalizar_venda();
+    resultado.then(function (response) {
+        alert("Venda finalizada. Total: $" + response.toFixed(2));
+        atualizarCarrinho();
+    });
 }
+
+function listarProdutos() {
+    var resultado = window.pywebview.api.listar_produtos();
+    resultado.then(function (produtos) {
+        var container = document.getElementById("produtos");
+        container.innerHTML = "";
+
+        produtos.forEach(function (produto) {
+            container.innerHTML += produto.nome + " (" + produto.codigo + "): $" + produto.preco + "<br>";
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementsByClassName('tablinks')[0].click();
+});
